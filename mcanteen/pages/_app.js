6152fc8a05ced233bdd7beae50aa932653f753ca
Manupal Choudhary <tech.manujpr@gmail.com>
2022-05-20 14:42:07 +0000

    Init

---
+++ mcanteen/pages/_app.js
@@ -0,0 +1,21 @@
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { App } from 'konsta/react';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    // Wrap our app with App component
+    <App theme="ios">
+      <Component {...pageProps} />
+    </App>
+  );
+}
+
+export default MyApp;
