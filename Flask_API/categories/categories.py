0c097c26ba7a4aa949ff3b66ee1d4f59e990af3b
Manupal Choudhary <tech.manujpr@gmail.com>
2022-05-22 13:09:12 +0000

    Categories Added

---
+++ Flask_API/categories/categories.py
@@ -0,0 +1,60 @@
+import os
+from fastapi import FastAPI, Request
+from fastapi import APIRouter
+router = APIRouter()
+
+from deta import Deta
+from dotenv import load_dotenv
+load_dotenv()
+deta = Deta(os.getenv('deta_key'))
+
+categories = deta.Base("categories")
+
+# Delete working
+@router.delete('/categories/{id}')
+async def deletecategories(id:str):
+    categories.delete(id)
+    return {
+        "status" : "SUCCESS",
+        "message":f"category with id {id} deleted"
+    }
+
+# Update working
+@router.put('/categories/{id}')
+async def updatecategories(id:str, request : Request):
+    try:
+        r = await request.json()        
+        categories.put({"category":r},id)        
+        return {
+            "status" : "SUCCESS",
+            "message":f"category with id {id} updated",
+            "data" : r
+        }
+    except:
+        return {
+            "status" : "ERROR",
+            "message":f"category with id {id} was not updated",
+        }
+
+
+@router.post('/categories/')
+async def postcategories(request : Request):
+    # id:str and category: str implies input validation, ie the url parameters can be of only type string
+    r = await request.json()
+    categories.insert({"product":r})
+    return {
+        "status" : "SUCCESS",
+        "message" : "category created successfully",
+        "data" : r
+    }
+
+
+@router.get('/categories/')
+async def getcategories():
+    respose={}
+    # fetches all the categorys from Deta cloud db
+    allcategories=categories.fetch()
+    for item in allcategories.items:
+        respose[item["key"]]=item["product"]
+    return respose
+    
--- Flask_API/main.py
+++ Flask_API/main.py
@@ -9,69 +9,19 @@ app.add_middleware(
         allow_origins=['*']
 )
 
-from deta import Deta
-from dotenv import load_dotenv
-load_dotenv()
-
-#deta = Deta("c0qm30mo_ZAEx1mTufH9EyGpDivRP3p56x1PxoqzY")
-deta = Deta(os.getenv('deta_key'))
-
-
 #app = Flask(__name__)
 from users.users import router as users_router
+from products.products import router as products_router
+
 app.include_router(users_router)
+app.include_router(products_router)
 
+# from deta import Deta
+# from dotenv import load_dotenv
+# load_dotenv()
 
-products = deta.Base("products")
+# #deta = Deta("c0qm30mo_ZAEx1mTufH9EyGpDivRP3p56x1PxoqzY")
+# deta = Deta(os.getenv('deta_key'))
 
-# Delete working
-@app.delete('/products/{id}')
-async def deleteProducts(id:str):
-    products.delete(id)
-    return {
-        "status" : "SUCCESS",
-        "message":f"user with id {id} deleted"
-    }
+# products = deta.Base("products")
 
-
-# Update working
-@app.put('/products/{id}')
-async def updateProducts(id:str, request : Request):
-    try:
-        r = await request.json()
-        print(r.get('empno'))    
-        products.put({"user":r},id)
-        #return {"message":f"user with id {id} updated"}
-        return {
-            "status" : "SUCCESS",
-            "message":f"user with id {id} updated",
-            "data" : r
-        }
-    except:
-        return {
-            "status" : "ERROR",
-            "message":f"user with id {id} was not updated",
-        }
-
-
-@app.post('/products/')
-async def postProducts(request : Request):
-    # id:str and user: str implies input validation, ie the url parameters can be of only type string
-    r = await request.json()
-    products.insert({"product":r})
-    return {
-        "status" : "SUCCESS",
-        "message" : "User created successfully",
-        "data" : r
-    }
-
-
-@app.get('/products/')
-async def getProducts():
-    respose={}
-    # fetches all the users from Deta cloud db
-    allproducts=products.fetch()
-    for item in allproducts.items:
-        respose[item["key"]]=item["product"]
-    return respose
-    
